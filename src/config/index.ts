c29b628d7c49b6ce4fec949e828bca2a8fb082db
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2021-01-13 10:03:39 -0300

    feat: add config

---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+export default {
+  googleKey: 'AIzaSyAakv60r4erxnsHoNTApJSM-f7HXegbotA',
+  openWeatherKey: '537b7ba3834a3962cc13096f0f885311',
+  googleURL: 'https://maps.googleapis.com/maps/api/geocode/json',
+  openWeatherURL: 'https://api.openweathermap.org/data/2.5/weather',
+}
