3f034337b841ebfa3b6f3d6a0889c473e3e5000c
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2021-01-13 11:21:56 -0300

    feat: add permition request

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native'
 import { registerRootComponent } from 'expo'
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
-import { useApp } from './hooks/app'
+import * as Location from 'expo-location';
 import Weather from './screens/Weather'
 import Provider from './hooks'
 
+type Coords = {
+  latitude: number,
+  longitude: number
+}
 const App:React.FC = () => {
-  const { loadData } = useApp()
+  const [location, setLocation] = useState({} as Coords);
 
- 
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') return
+      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({});
+      setLocation({latitude, longitude})
+    })();
+  }, []);
+
+  if(!location.latitude) {
+    return <View><Text>Você não pode ver os dados climáticos</Text></View>
+  }
 
   return (
     <Provider >
